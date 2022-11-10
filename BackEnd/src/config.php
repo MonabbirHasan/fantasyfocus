d0de9456d3929c9bfac5d60f3aea36b2b9157d98
monabbirhasan <phpjsmonabbirhasan@gmai.com>
2022-11-10 21:00:18 +0600

    fantasyfocuse

---
+++ BackEnd/src/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+$conn=mysqli_connect("localhost","root","","fantasyfocus") or die("database is not connected");
+
+// if($conn){
+// 	echo "database is connected";
+// }else{
+// 	echo "database is not connected";
+// }
+
+ ?>
